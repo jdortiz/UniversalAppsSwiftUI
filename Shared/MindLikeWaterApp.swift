0cf6fbad25392598432bf164c6316f61c05935c4
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2021-11-17 10:08:37 +0100

    Initial Commit

---
+++ Shared/MindLikeWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindLikeWaterApp.swift
+//  Shared
+//
+//  Created by Jorge Ortiz Fuentes on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MindLikeWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
