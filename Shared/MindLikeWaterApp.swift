9604b667b1e7c461d2cec87b7fb79dae097ba542
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2021-11-18 09:18:59 +0100

    Add NavigationView
    - Rename ContentView to say what it now is.
    - Add NavigationView to preview also.

--- Shared/MindLikeWaterApp.swift
+++ Shared/MindLikeWaterApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct MindLikeWaterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ActionsListView()
+            }
         }
     }
 }
